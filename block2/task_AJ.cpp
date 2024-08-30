6c547899f88ecaed76f2fb54d492bb79653a7308
Vitiks <130283182+Vitiks-official@users.noreply.github.com>
2024-08-30 20:27:49 +0300

    Create task_AJ.cpp

---
+++ block2/task_AJ.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    switch(num){
+        case 1: cout << "YAN"; break;
+        case 2: cout << "FEV"; break;
+        case 3: cout << "MAR"; break;
+        case 4: cout << "APR"; break;
+        case 5: cout << "MAI"; break;
+        case 6: cout << "IUN"; break;
+        case 7: cout << "IUL"; break;
+        case 8: cout << "AVG"; break;
+        case 9: cout << "SEN"; break;
+        case 10: cout << "OKT"; break;
+        case 11: cout << "NOV"; break;
+        case 12: cout << "DEC"; break;
+    }
+}
