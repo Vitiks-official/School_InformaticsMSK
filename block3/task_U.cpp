0f331f01d0f8f4ddfe4f57890610a2a9fa398602
Vitiks <130283182+Vitiks-official@users.noreply.github.com>
2024-08-30 20:41:21 +0300

    Create task_U.cpp

---
+++ block3/task_U.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num, some_num = 0;
+    cin >> num;
+    while (num != 0){
+        if (num % 2 == 0){
+            some_num += 1;
+        }
+        cin >> num;
+    }
+    cout << some_num;
+}
