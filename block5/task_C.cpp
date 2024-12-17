a7e5503f6ac2e92457d6758612e6e4a2842731a0
Vitiks <vitiks78youtub@gmail.com>
2024-12-17 20:12:56 +0300

    add task_C.cpp

---
+++ block5/task_C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, k = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i){
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n - 1; ++i){
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]){
+            ++k;
+        }
+    }
+    cout << k;
+}
