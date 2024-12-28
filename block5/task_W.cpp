f74c1261e0407d5a2e4ad8549fec64965a537762
Vitiks <vitiks78youtub@gmail.com>
2024-12-29 00:41:36 +0300

    add task_W.cpp

---
+++ block5/task_W.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    bool flag = false, any = false;
+    cin >> n >> m;
+    for (int i = n; i <= m; ++i){
+        for (int j = 2; j * j <= i; ++j){
+            if (i % j == 0){
+                flag = true;
+                break;
+            }
+        }
+        if (!flag){
+            cout << i << "\n";
+            any = true;
+        }
+        flag = false;
+    }
+    if (!any)
+        cout << "Absent";
+}
